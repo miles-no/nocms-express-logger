ffb74a070fd79a25d293cf37466e439ca3c3b66a
Jon Arild Tørresdal <jon@torresdal.net>
2017-06-10 21:34:15 +0200

    Create express middleware logger for nocms

---
+++ README.md
@@ -0,0 +1,8 @@
+# Express logger middleware package for NoCMS
+
+## Usage
+
+```
+const expressLogger = require('nocms-express-logger');
+app.use(expressLogger());
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const bunyan = require('bunyan');
+
+module.exports = function(options) {
+  const logger = bunyan.createLogger({ name: 'nocms-express' });
+
+  return function(req, res, next) {
+    var start = process.hrtime()
+
+    let obj = {
+      express: {
+        req: {
+          method: req.method,
+          url: req.originalUrl || req.url,
+          user: req.user || '-',
+          query: req.query,
+          httpVersion: `${req.httpVersionMajor}.${req.httpVersionMinor}`,
+          remoteAddress: req.connection.remoteAddress,
+          remotePort: req.connection.remotePort
+        }
+      }
+    };
+
+    res.on('finish', function() {
+      obj.express.res = {
+        statusCode: res.statusCode,
+        contentLength: res.get("content-length"),
+        duration: getDuration(start)
+      };
+      logger.info(obj, `${obj.express.req.remoteAddress} ${obj.express.req.user} HTTP/${obj.express.req.httpVersion} ${obj.express.req.method} ${obj.express.req.url} ${obj.express.res.statusCode} ${obj.express.res.contentLength} ${obj.express.res.duration} ms`);
+    });
+
+    res.on('close', function () {
+      obj.express.res = {
+        statusCode: res.statusCode,
+        contentLength: res.get("content-length"),
+        duration: getDuration(start)
+      };
+      logger.warn(obj, `${obj.express.req.remoteAddress} ${obj.express.req.user} HTTP/${obj.express.req.httpVersion} ${obj.express.req.method} ${obj.express.req.url} ${obj.express.res.statusCode} ${obj.express.res.contentLength} ${obj.express.res.duration} ms`);
+    });
+
+    next()
+  }
+
+  function getDuration(start) {
+    var diff = process.hrtime(start);
+    return diff[0] * 1e3 + diff[1] * 1e-6;
+  }
+}
