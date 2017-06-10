3238bb661aeb85e924ef1839877b10b585c46289
Jon Arild Tørresdal <jon@torresdal.net>
2017-06-11 00:18:41 +0200

    Added support for correlation id

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 'use strict';
 
 const bunyan = require('bunyan');
+const correlator = require('correlation-id');
 
 module.exports = function(options) {
   const logger = bunyan.createLogger({ name: 'nocms-express' });
 
   return function(req, res, next) {
-    var start = process.hrtime()
+    const start = process.hrtime()
+    const id = req.get('x-correlation-id');
+
+    if (id) {
+      correlator.withId(id);
+    }
 
     let obj = {
       express: {
+        correlationId: correlator.getId(),
         req: {
           method: req.method,
           url: req.originalUrl || req.url,
-          user: req.user || '-',
+          user: req.user || '',
           query: req.query,
           httpVersion: `${req.httpVersionMajor}.${req.httpVersionMinor}`,
           remoteAddress: req.connection.remoteAddress,
@@ -25,7 +32,7 @@ module.exports = function(options) {
     res.on('finish', function() {
       obj.express.res = {
         statusCode: res.statusCode,
-        contentLength: res.get("content-length"),
+        contentLength: res.get("content-length") || '',
         duration: getDuration(start)
       };
       logger.info(obj, `${obj.express.req.remoteAddress} ${obj.express.req.user} HTTP/${obj.express.req.httpVersion} ${obj.express.req.method} ${obj.express.req.url} ${obj.express.res.statusCode} ${obj.express.res.contentLength} ${obj.express.res.duration} ms`);
@@ -34,7 +41,7 @@ module.exports = function(options) {
     res.on('close', function () {
       obj.express.res = {
         statusCode: res.statusCode,
-        contentLength: res.get("content-length"),
+        contentLength: res.get("content-length") || '',
         duration: getDuration(start)
       };
       logger.warn(obj, `${obj.express.req.remoteAddress} ${obj.express.req.user} HTTP/${obj.express.req.httpVersion} ${obj.express.req.method} ${obj.express.req.url} ${obj.express.res.statusCode} ${obj.express.res.contentLength} ${obj.express.res.duration} ms`);
