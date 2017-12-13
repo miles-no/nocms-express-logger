019b8ebd93e21d47aef519100e42a71a9df55e16
thomastho <thomas.thorbjornsen@gmail.com>
2017-12-13 13:51:38 +0100

    feat: remove bunyan, added logger as argument BREAKING CHANGE you must now send logger as argument

--- index.js
+++ index.js
@@ -1,52 +1,28 @@
-'use strict';
+const getDuration = (start) => {
+  const diff = process.hrtime(start);
+  return (diff[0] * 1e3) + (diff[1] * 1e-6);
+};
 
-const bunyan = require('bunyan');
-const correlator = require('correlation-id');
+const getMessage = (req, res, duration) => {
+  const contentLength = res.get('content-length') || '';
 
-module.exports = function(options) {
-  const logger = bunyan.createLogger({ name: 'nocms-express' });
+  return `${req.method} ${req.originalUrl || req.url} ${res.statusCode} ${contentLength} ${duration} ms`;
+};
 
-  return function(req, res, next) {
-    const start = process.hrtime()
 
-    let obj = {
-      express: {
-        correlationId: req.get('x-correlation-id'),
-        req: {
-          method: req.method,
-          url: req.originalUrl || req.url,
-          user: req.user || '',
-          query: req.query || '',
-          httpVersion: `${req.httpVersionMajor}.${req.httpVersionMinor}`,
-          remoteAddress: req.connection.remoteAddress,
-          remotePort: req.connection.remotePort
-        }
-      }
-    };
-
-    res.on('finish', function() {
-      obj.express.res = {
-        statusCode: res.statusCode,
-        contentLength: res.get("content-length") || '',
-        duration: getDuration(start)
-      };
-      logger.info(obj, `${obj.express.req.remoteAddress} ${obj.express.req.user} HTTP/${obj.express.req.httpVersion} ${obj.express.req.method} ${obj.express.req.url} ${obj.express.res.statusCode} ${obj.express.res.contentLength} ${obj.express.res.duration} ms`);
+module.exports = (logger = console) => {
+  const middleware = (req, res, next) => {
+    const start = process.hrtime();
+    res.on('finish', () => {
+      logger.info(getMessage(req, res, getDuration(start)), { req, res }, 'express');
     });
 
-    res.on('close', function () {
-      obj.express.res = {
-        statusCode: res.statusCode,
-        contentLength: res.get("content-length") || '',
-        duration: getDuration(start)
-      };
-      logger.warn(obj, `${obj.express.req.remoteAddress} ${obj.express.req.user} HTTP/${obj.express.req.httpVersion} ${obj.express.req.method} ${obj.express.req.url} ${obj.express.res.statusCode} ${obj.express.res.contentLength} ${obj.express.res.duration} ms`);
+    res.on('close', () => {
+      logger.warn(getMessage(req, res, getDuration(start)), { req, res }, 'express');
     });
 
-    next()
-  }
+    next();
+  };
 
-  function getDuration(start) {
-    var diff = process.hrtime(start);
-    return diff[0] * 1e3 + diff[1] * 1e-6;
-  }
-}
+  return middleware;
+};
